61a1089467e036e7cd4c3b2191c2378d12aec25d
MuhammadAbdullahSyed <mohd.abdullah07@gmail.com>
2024-04-03 21:26:45 +0500

    develop number guessing game

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import inquier from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquier.prompt([{
+        name: "UserGuessNumber",
+        type: "number",
+        messange: "Enter your guess number between 1 to 6",
+    }]);
+if (answer.UserGuessNumber === randomNumber) {
+    console.log("Congratulation you enter correct number");
+}
+else {
+    console.log("Please try again!");
+}
